ecf4245df0bba9d2d5f53f9a7f31c30c5b7dfd90
Srinivasa Samineni <ssamineni@indusrad.com>
2024-08-30 08:18:07 -0400

    Initial Test cloudflare

---
+++ radixone/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.indusrad.radixone',
+  appName: 'radixone',
+  webDir: 'dist',
+  bundledWebRuntime: true
+};
+
+export default config;
