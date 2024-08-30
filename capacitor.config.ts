a888fb42e6bab32866f6bab67b54e70201e44ef6
srsirad <ssamineni@indusrad.com>
2024-08-30 08:42:40 -0400

    test3

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.indusrad.radixone',
+  appName: 'radixone',
+  webDir: 'dist'
+};
+
+export default config;
